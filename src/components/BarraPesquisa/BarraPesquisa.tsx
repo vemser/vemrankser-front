c317a3b804cd4ade9d5ab1e656330ecdb6302bd7
Luiza Valentini <luizadaravalentini@gmail.com>
2022-11-27 16:40:16 -0300

    arrumando o style

--- src/components/BarraPesquisa/BarraPesquisa.tsx
+++ src/components/BarraPesquisa/BarraPesquisa.tsx
@@ -1,10 +1,26 @@
 import TextField from "@mui/material/TextField";
+import { styled } from "@mui/material/styles";
 import { IBarraPesquisa } from "../../types/barraDePesquisa";
+
+const TextFieldStyle = styled(TextField)({
+  heigth: 50,
+  "& label.Mui-focused": {
+    color: "var(--cor-primaria)",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "var(--cor-primaria)",
+  },
+  "& .MuiOutlinedInput-root": {
+    "&.Mui-focused fieldset": {
+      borderColor: "var(--cor-primaria)",
+    },
+  },
+});
 
 export default function BarraPesquisa({ label, id }: IBarraPesquisa) {
   return (
     <>
-      <TextField id={id} label={label} variant="standard" />
+      <TextFieldStyle id={id} label={label} variant="standard" />
     </>
   );
 }
